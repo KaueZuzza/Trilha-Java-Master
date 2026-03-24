17bbcfb2f68d055e3a09b42860c36179f8d82e9b
KaueZuzza <kauezuzzaw@gmail.com>
2026-03-24 02:10:46 -0300

    organizando pastas por níveis e iniciando POO

--- src/main/java/org/example/nivel02_poo/TestarAluno.java
+++ src/main/java/org/example/nivel02_poo/TestarAluno.java
@@ -2,7 +2,7 @@ package org.example.nivel02_poo;
 
 public class TestarAluno {
     public static void main(String[] args) {
-        // Aqui você cria os objetos baseados no molde (Aluno)
+        // objetos  molde (Aluno)
         Aluno aluno1 = new Aluno("Kaue", 9.5);
         Aluno aluno2 = new Aluno("Joao", 5.0);
 
